29df8775573ab6e72674d2df6d75ffaffaa08c9e
BrunoChauvet <bruno.chauvet@live.fr>
2014-10-11 21:55:29 +1100

    receive invoices updates

--- maestrano/data/subscribe.php
+++ maestrano/data/subscribe.php
@@ -1,50 +1,50 @@
-<?php
+  <?php
 
-//-----------------------------------------------
-// Define root folder
-//-----------------------------------------------
-if (!defined('MAESTRANO_ROOT')) {
-  define("MAESTRANO_ROOT", realpath(dirname(__FILE__) . '/../'));
-}
+  //-----------------------------------------------
+  // Define root folder
+  //-----------------------------------------------
+  if (!defined('MAESTRANO_ROOT')) {
+    define("MAESTRANO_ROOT", realpath(dirname(__FILE__) . '/../'));
+  }
 
-require_once(MAESTRANO_ROOT . '/app/init/soa.php');
+  require_once(MAESTRANO_ROOT . '/app/init/soa.php');
 
-$maestrano = MaestranoService::getInstance();
+  $maestrano = MaestranoService::getInstance();
 
-if ($maestrano->isSoaEnabled() and $maestrano->getSoaUrl()) {
-    $log = new MnoSoaBaseLogger();
+  if ($maestrano->isSoaEnabled() and $maestrano->getSoaUrl()) {
+      $log = new MnoSoaBaseLogger();
 
-    $notification = json_decode(file_get_contents('php://input'), false);
-    $notification_entity = strtoupper(trim($notification->entity));
-    
-    $log->debug("Notification = ". json_encode($notification));
-    
-    switch ($notification_entity) {
-	    case "ORGANIZATIONS":
-                if (class_exists('MnoSoaOrganization')) {
-                    $mno_org = new MnoSoaOrganization($opts['db_connection'], $log);		
-                    $mno_org->receiveNotification($notification);
-                }
-				break;
+      $notification = json_decode(file_get_contents('php://input'), false);
+      $notification_entity = strtoupper(trim($notification->entity));
+      
+      $log->debug("Notification = ". json_encode($notification));
+      
+      switch ($notification_entity) {
+  	    case "ORGANIZATIONS":
+          if (class_exists('MnoSoaOrganization')) {
+              $mno_org = new MnoSoaOrganization($opts['db_connection'], $log);		
+              $mno_org->receiveNotification($notification);
+          }
+				  break;
         case "PERSONS":
-                if (class_exists('MnoSoaPerson')) {
-                    $mno_person = new MnoSoaPerson($opts['db_connection'], $log);       
-                    $mno_person->receiveNotification($notification);
-                }
-                break;
+          if (class_exists('MnoSoaPerson')) {
+              $mno_person = new MnoSoaPerson($opts['db_connection'], $log);       
+              $mno_person->receiveNotification($notification);
+          }
+          break;
         case "ITEMS":
-                if (class_exists('MnoSoaItem')) {
-                    $mno_item = new MnoSoaItem($opts['db_connection'], $log);       
-                    $mno_item->receiveNotification($notification);
-                }
-                break;
+          if (class_exists('MnoSoaItem')) {
+              $mno_item = new MnoSoaItem($opts['db_connection'], $log);       
+              $mno_item->receiveNotification($notification);
+          }
+          break;
         case "INVOICES":
-                if (class_exists('MnoSoaInvoice')) {
-                    $mno_invoice = new MnoSoaInvoice($opts['db_connection'], $log);       
-                    $mno_invoice->receiveNotification($notification);
-                }
-                break;
-    }
-}
+          if (class_exists('MnoSoaInvoice')) {
+              $mno_invoice = new MnoSoaInvoice($opts['db_connection'], $log);       
+              $mno_invoice->receiveNotification($notification);
+          }
+          break;
+      }
+  }
 
-?>
+  ?>
