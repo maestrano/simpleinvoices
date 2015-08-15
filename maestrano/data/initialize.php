27f3ab100d428534275900a0d1bcddf0df0e3266
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2015-08-16 06:29:20 +1000

    rework data endpoints

--- maestrano/data/initialize.php
+++ maestrano/data/initialize.php
@@ -1,33 +1,43 @@
 <?php
 
-//-----------------------------------------------
-// Define root folder
-//-----------------------------------------------
-if (!defined('MAESTRANO_ROOT')) {
-  define("MAESTRANO_ROOT", realpath(dirname(__FILE__) . '/../'));
+require_once '../init.php';
+require_once '../connec/init.php';
+
+if(!Maestrano::param('connec.enabled')) { return false; }
+
+$filepath = '../var/_data_sequence';
+$status = false;
+
+if (file_exists($filepath)) {
+  // Last update timestamp
+  $timestamp = trim(file_get_contents($filepath));
+  $current_timestamp = round(microtime(true) * 1000);
+  if (empty($timestamp)) { $timestamp = 0; }
+
+  // Fetch updates
+  $client = new Maestrano_Connec_Client();
+  $msg = $client->get("updates/$timestamp?\$filter[entity]=Company,TaxCode,Organization,Person,Item,Invoice,Payment");
+  $code = $msg['code'];
+  $body = $msg['body'];
+
+  if($code != 200) {
+    error_log("Cannot fetch connec updates code=$code, body=$body");
+  } else {
+    error_log("Receive updates body=$body");
+    $result = json_decode($body, true);
+
+    // Dynamically find mappers and map entities
+    foreach(BaseMapper::getMappers() as $mapperClass) {
+      $mapper = new $mapperClass();
+      $mapper->persistAll($result[$mapper->getConnecResourceName()]);
+    }
+  }
+
+  $status = true;
 }
 
-require_once(MAESTRANO_ROOT . '/app/init/soa.php');
-
-$maestrano = MaestranoService::getInstance();
-
-if ($maestrano->isSoaEnabled() and $maestrano->getSoaUrl()) {
-    $filepath = MAESTRANO_ROOT . '/var/_data_sequence';
-    $status = false;
-    
-    if (file_exists($filepath)) {
-        $timestamp = trim(file_get_contents($filepath));
-        $current_timestamp = round(microtime(true) * 1000);
-        
-        if (empty($timestamp)) { $timestamp = 0; } 
-
-        $mno_entity = new MnoSoaEntity($opts['db_connection'], new MnoSoaBaseLogger());
-        $status = $mno_entity->getUpdates($timestamp);
-    }
-    
-    if ($status) {
-        file_put_contents($filepath, $current_timestamp);
-    }
+if ($status) {
+  file_put_contents($filepath, $current_timestamp);
 }
 
 ?>
--- maestrano/data/subscribe.php
+++ maestrano/data/subscribe.php
@@ -1,74 +1,49 @@
-  <?php
+<?php
 
-  //-----------------------------------------------
-  // Define root folder
-  //-----------------------------------------------
-  if (!defined('MAESTRANO_ROOT')) {
-    define("MAESTRANO_ROOT", realpath(dirname(__FILE__) . '/../'));
+require_once '../init.php';
+require_once '../connec/init.php';
+
+try {
+  if(!Maestrano::param('connec.enabled')) { return false; }
+
+  $notification = json_decode(file_get_contents('php://input'), false);
+  $entity_name = strtoupper(trim($notification->entity));
+  $entity_id = $notification->id;
+
+  error_log("Received notification = ". json_encode($notification));
+
+  switch ($entity_name) {
+    case "COMPANYS":
+      $mapper = new CompanyMapper();
+      $mapper->fetchConnecResource($entity_id);
+      break;
+    case "TAXCODES":
+      $mapper = new TaxCodeMapper();
+      $mapper->fetchConnecResource($entity_id);
+      break;
+    case "ORGANIZATIONS":
+      $mapper = new OrganizationMapper();
+      $mapper->fetchConnecResource($entity_id);
+      break;
+    case "PEOPLE":
+      $mapper = new PersonMapper();
+      $mapper->fetchConnecResource($entity_id);
+      break;
+    case "ITEMS":
+      $mapper = new ItemMapper();
+      $mapper->fetchConnecResource($entity_id);
+      break;
+    case "INVOICES":
+      $mapper = new InvoiceMapper();
+      $mapper->fetchConnecResource($entity_id);
+      break;
+    case "PAYMENTS":
+      $mapper = new PaymentMapper();
+      $mapper->fetchConnecResource($entity_id);
+      break;
   }
+} catch (Exception $e) {
+  error_log("Caught exception in subscribe " . json_encode($e->getMessage()));
+}
 
-  require_once(MAESTRANO_ROOT . '/app/init/soa.php');
-
-  $maestrano = MaestranoService::getInstance();
-
-  if ($maestrano->isSoaEnabled() and $maestrano->getSoaUrl()) {
-      $log = new MnoSoaBaseLogger();
-
-      $notification = json_decode(file_get_contents('php://input'), false);
-      $notification_entity = strtoupper(trim($notification->entity));
-      
-      $log->debug("Notification = ". json_encode($notification));
-      
-      switch ($notification_entity) {
-  	    case "COMPANY":
-          if (class_exists('MnoSoaCompany')) {
-            $mno_company = new MnoSoaCompany($opts['db_connection'], $log);
-            $mno_company->receiveNotification($notification);
-          }
-          break;
-        case "TAXCODES":
-              if (class_exists('MnoSoaTax')) {
-                $mno_tax = new MnoSoaTax($opts['db_connection'], $log);
-                $mno_tax->receiveNotification($notification);
-              }
-          break;
-        case "ORGANIZATIONS":
-          if (class_exists('MnoSoaOrganization')) {
-              $mno_org = new MnoSoaOrganization($opts['db_connection'], $log);    
-              $mno_org->receiveNotification($notification);
-          }
-          break;
-        case "ORGANIZATIONS":
-          if (class_exists('MnoSoaOrganization')) {
-              $mno_org = new MnoSoaOrganization($opts['db_connection'], $log);    
-              $mno_org->receiveNotification($notification);
-          }
-          break;
-        case "PERSONS":
-          if (class_exists('MnoSoaPerson')) {
-              $mno_person = new MnoSoaPerson($opts['db_connection'], $log);       
-              $mno_person->receiveNotification($notification);
-          }
-          break;
-        case "ITEMS":
-          if (class_exists('MnoSoaItem')) {
-              $mno_item = new MnoSoaItem($opts['db_connection'], $log);       
-              $mno_item->receiveNotification($notification);
-          }
-          break;
-        case "INVOICES":
-          if (class_exists('MnoSoaInvoice')) {
-              $mno_invoice = new MnoSoaInvoice($opts['db_connection'], $log);       
-              $mno_invoice->receiveNotification($notification);
-          }
-          break;
-        case "PAYMENTS":
-          if (class_exists('MnoSoaPayment')) {
-              $mno_payment = new MnoSoaPayment($opts['db_connection'], $log);       
-              $mno_payment->receiveNotification($notification);
-          }
-          break;
-      }
-  }
-
-  ?>
+?>
